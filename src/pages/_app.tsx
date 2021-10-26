4bfb750cd5f5749d7818df4b3ea70a47538b6176
GuiSantosDev <guisantos.dev@outlook.com>
2021-10-26 01:09:36 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../styles/global.scss";
+import type { AppProps } from "next/app";
+import { Header } from "../components/Header";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
+}
+export default MyApp;
