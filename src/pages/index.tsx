6151b945ab8f0648a41840584496d5e56ff28708
GuiSantosDev <guisantos.dev@outlook.com>
2021-10-28 11:53:46 -0300

    refactor: _app and index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  return (
-    <h1>index
-    </h1>
-  );
+interface HomePros {
+  episodes: JSON;
+}
+
+const Home: NextPage<HomePros> = ({ episodes }) => {
+  return <h1>{JSON.stringify(episodes)}</h1>;
 };
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
 
 export default Home;
